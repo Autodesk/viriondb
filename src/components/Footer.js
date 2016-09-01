31681feb95f1dbb36b32317252fcb024a096415d
Maxwell Bates <max.bates@autodesk.com>
2016-09-01 13:01:43 -0700

    add a footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,20 @@ import '../styles/Footer.css';
 export default class Footer extends Component {
   render() {
     return (
-      <div className="Footer"></div>
+      <div className="Footer">
+        <div className="Footer-autodesk"/>
+        <div className="Footer-right">
+          <div className="Footer-links">
+            <a className="Footer-link">About VirionDB</a>
+            <a className="Footer-link">FAQ</a>
+            <a className="Footer-link">Community</a>
+            <a className="Footer-link">Feedback</a>
+            <a className="Footer-link">Support</a>
+            <a className="Footer-link">More Bio Nano Tools</a>
+          </div>
+          <div className="Footer-logo"/>
+        </div>
+      </div>
     );
   }
 };
