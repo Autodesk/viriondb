55a98545829c3ae407228943d7098c18e51c7056
Maxwell Bates <max.bates@autodesk.com>
2016-09-06 13:47:13 -0700

    add budded (sphere) 3d type, note on protein to link to ncbi

--- src/components/rowDetail/Proteins.js
+++ src/components/rowDetail/Proteins.js
@@ -5,25 +5,31 @@ import MoleculeViewer from '../MoleculeViewer';
 export default class Proteins extends Component {
   static propTypes = {
     value: PropTypes.string.isRequired,
+    instance: PropTypes.object.isRequired,
   };
 
   makeUrl = () => {
     return `/`;
   };
 
-  render() {
-    //todo - verify this
+  componentWillUpdate(nextProps) {
+    this.ncbiLink = null;
+
     const id = this.props.instance.id;
-    const nuccore = `https://www.ncbi.nlm.nih.gov/nuccore/${id}`;
 
-    //todo - not real. need UUID from genbank, or to run an eSearch and find these
-    const proteins = `https://www.ncbi.nlm.nih.gov/protein/${id}`;
+    //todo - fetch and set ncbiLink for proteins
+  }
 
+  render() {
     const moleculeViewer = null; //<MoleculeViewer url={this.makeUrl()}/>
 
     return (
       <div className="Proteins">
         {moleculeViewer}
+
+        {this.ncbiLink && <a className="ComparisonRow-link ComparisonRow-offsite"
+           href={this.ncbiLink}
+           target="_blank">NCBI Proteins</a>
       </div>
     );
   }
