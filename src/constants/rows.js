5249567deb28387569481e5b9bea5f87e19927bc
Maxwell Bates <maxbates@gmail.com>
2016-09-07 14:49:56 -0700

    Update rows.js

--- src/constants/rows.js
+++ src/constants/rows.js
@@ -31,7 +31,7 @@ export const rowNames = {
   derived_sense: 'Sense',
   sequenced_mol: 'Nucleic Acid Molecule',
   nucleid_acid_structure: 'Nucleic Acid Structure',
-  nucleic_acid_shape: 'Nucleid Acid Shape',
+  nucleic_acid_shape: 'Nucleic Acid Shape',
   genome_shape: 'Genome Shape',
   derived_protein_count: 'Protein Count',
   derived_segment_count: 'Segments',
